911398d66ee060aa77b0e6b6718d5b33d6e1ee2f
mslalith <santosh2397@gmail.com>
2022-03-20 13:30:01 +0530

    add Maven Publishing to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
     kotlin("jvm") version "1.6.10"
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 group = "dev.mslalith"
-version = "1.0-SNAPSHOT"
+version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -21,4 +24,71 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
     testImplementation("app.cash.turbine:turbine:0.7.0")
     testImplementation("com.google.truth:truth:1.1.3")
+}
+
+object Meta {
+    const val description = "Poller is a simple Kotlin library which runs a certain task at a regular interval"
+    const val license = "Apache-2.0"
+    const val githubRepo = "mslalith/poller"
+    const val release = "https://s01.oss.sonatype.org/service/local/"
+    const val snapshot = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}
+
+publishing {
+    repositories {
+        maven {
+
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["kotlin"])
+            pom {
+                name.set(project.name)
+                description.set(Meta.description)
+                url.set("https://github.com/${Meta.githubRepo}")
+                licenses {
+                    license {
+                        name.set(Meta.license)
+                        url.set("https://github.com/${Meta.githubRepo}/blob/main/LICENSE.md")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("mslalith")
+                        name.set("M S Lalith")
+                        organization.set("M S Lalith")
+                        organizationUrl.set("https://mslalith.dev")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/${Meta.githubRepo}.git")
+                    connection.set("scm:git:git://github.com/${Meta.githubRepo}.git")
+                    developerConnection.set("scm:git:git://github.com/${Meta.githubRepo}.git")
+                }
+                issueManagement {
+                    url.set("https://github.com/${Meta.githubRepo}/issues")
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri(Meta.release))
+            snapshotRepositoryUrl.set(uri(Meta.snapshot))
+            val ossrhUsername = providers.environmentVariable("OSSRH_USERNAME").forUseAtConfigurationTime()
+            val ossrhPassword = providers.environmentVariable("OSSRH_PASSWORD").forUseAtConfigurationTime()
+            if (ossrhUsername.isPresent && ossrhPassword.isPresent) {
+                username.set(ossrhUsername.get())
+                password.set(ossrhPassword.get())
+            }
+        }
+    }
 }
